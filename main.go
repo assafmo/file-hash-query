2a05a0ff744f29df2b7745f1d793af9b2f8460a1
assafmo <assaf.morami@gmail.com>
2016-12-07 21:27:17 +0200

    virustotal works

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"regexp"
+	"strings"
+
+	"golang.org/x/net/html"
+
+	"gopkg.in/xmlpath.v2"
+)
+
+func main() {
+	sha256 := flag.String("sha256", "", "sha256 hash of a file")
+	flag.Parse()
+
+	sha256Regex := regexp.MustCompile("^[a-fA-F0-9]{64}$")
+
+	if !sha256Regex.MatchString(*sha256) {
+		log.Fatalln("cannot match sha256 with regex ^[a-fA-F0-9]{64}$")
+	}
+
+	vt, _ := virustotal(*sha256)
+
+	fmt.Println("Result:", vt)
+}
+
+func virustotal(sha256 string) (string, error) {
+	request, err := http.NewRequest("GET", fmt.Sprintf("https://www.virustotal.com/en/file/%s/analysis/", sha256), nil)
+	if err != nil {
+		return "", err
+	}
+
+	request.Header.Set("user-agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Mobile Safari/537.36")
+	request.Header.Set("referer", "https://google.com/")
+	request.Header.Set("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+	request.Header.Set("accept-language", "en-US,en;q=0.8,he;q=0.6")
+	request.Header.Set("cache-control", "max-age=0")
+	request.Header.Set("dnt", "1")
+	request.Header.Set("upgrade-insecure-requests", "1")
+
+	response, err := new(http.Client).Do(request)
+	if err != nil {
+		return "", err
+	}
+
+	//fix html
+	rootHTML, err := html.Parse(response.Body)
+	defer response.Body.Close()
+	if err != nil {
+		return "", err
+	}
+
+	var b bytes.Buffer
+	html.Render(&b, rootHTML)
+	fixedHTML := b.String()
+
+	reader := strings.NewReader(fixedHTML)
+	xmlRoot, err := xmlpath.ParseHTML(reader)
+	if err != nil {
+		return "", err
+	}
+
+	scoreXPath := xmlpath.MustCompile(`//*[@id="basic-info"]/div/div[1]/table/tbody/tr[3]/td[2]`)
+	if value, ok := scoreXPath.String(xmlRoot); ok {
+		return strings.Trim(value, " \n"), nil
+	}
+
+	return "", fmt.Errorf("Result not found in HTML")
+}
