5ba28c0298c9515c6bb7b3b0a5de61f4fb35f3d3
assafmo <assaf.morami@gmail.com>
2016-12-08 08:39:33 +0200

    malwr extract csrftoken

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 		log.Fatalln("cannot match sha256 with regex ^[a-fA-F0-9]{64}$")
 	}
 
-	vt, err := virustotal(*sha256)
+	vt, err := malwr(*sha256)
 
 	if err != nil {
 		log.Fatalln(err)
@@ -70,4 +70,60 @@ func virustotal(sha256 string) (string, error) {
 	}
 
 	return "", fmt.Errorf("Not found on VirusTotal")
+}
+
+func malwr(sha256 string) (string, error) {
+	response, err := http.Head("https://malwr.com/analysis/search/")
+	if err != nil {
+		return "", err
+	}
+
+	cookies := response.Header.Get("Set-Cookie")
+	csrftokenRegex := regexp.MustCompile(`csrftoken.+?;`)
+
+	csrftokenMatches := csrftokenRegex.FindAllString(cookies, 1)
+	if len(csrftokenMatches) != 1 {
+		return "", fmt.Errorf("No csrftoken found")
+	}
+
+	csrftoken := csrftokenMatches[0][10 : len(csrftokenMatches[0])-1]
+	return csrftoken, nil
+	// url := fmt.Sprintf("https://www.virustotal.com/en/file/%s/analysis/", sha256)
+	// request, err := http.NewRequest("GET", url, nil)
+	// if err != nil {
+	// 	return "", err
+	// }
+
+	// request.Header.Set("user-agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Mobile Safari/537.36")
+	// request.Header.Set("referer", "https://malwr.com/analysis/search/")
+	// request.Header.Set("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+	// request.Header.Set("accept-language", "en-US,en;q=0.8,he;q=0.6")
+	// request.Header.Set("cache-control", "max-age=0")
+	// request.Header.Set("dnt", "1")
+	// request.Header.Set("upgrade-insecure-requests", "1")
+
+	// response, err = new(http.Client).Do(request)
+	// if err != nil {
+	// 	return "", err
+	// }
+
+	// xmlRoot, err := xmlpath.ParseHTML(response.Body)
+	// defer response.Body.Close()
+	// if err != nil {
+	// 	return "", err
+	// }
+
+	// scoreXPath := xmlpath.MustCompile(`//*[@id="basic-info"]/div/div[1]/table/tbody/tr[3]/td[2]`)
+	// dateXPath := xmlpath.MustCompile(`//*[@id="basic-info"]/div/div[1]/table/tbody/tr[4]/td[2]`)
+	// score, okScore := scoreXPath.String(xmlRoot)
+	// date, okDate := dateXPath.String(xmlRoot)
+
+	// if okDate && okScore {
+	// 	score = strings.Replace(strings.Trim(score, " \n"), " ", "", -1)
+	// 	date = strings.Trim(date, " \n")[:19]
+
+	// 	return fmt.Sprintf("score: %s, date: %s", score, date), nil
+	// }
+
+	// return "", fmt.Errorf("Not found on VirusTotal")
 }
