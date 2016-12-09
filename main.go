d3b6c073a44f53e4b277bec10ed0997fe4f46eca
assafmo <assaf.morami@gmail.com>
2016-12-09 14:23:53 +0200

    virustotal source file

--- main.go
+++ main.go
@@ -4,15 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net/http"
 	"regexp"
-	"strconv"
-
-	"strings"
 
 	"encoding/json"
-
-	"gopkg.in/xmlpath.v2"
 )
 
 func main() {
@@ -25,9 +19,17 @@ func main() {
 		log.Fatalln("Cannot match sha256 with regex ^[a-fA-F0-9]{64}$")
 	}
 
-	result, _ := virustotal(*sha256)
+	result, err := virustotal{}.CheckSHA256(*sha256)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	resultJSON, _ := json.MarshalIndent(result, "", "\t")
 	fmt.Println(string(resultJSON))
+}
+
+type source interface {
+	CheckSHA256(string) (reputation, error)
 }
 
 type reputation struct {
@@ -35,73 +37,4 @@ type reputation struct {
 	Good, Bad, Known, Unknown bool
 	Confidence                float64
 	Date                      string
-}
-
-func virustotal(sha256 string) (reputation, error) {
-	url := fmt.Sprintf("https://www.virustotal.com/en/file/%s/analysis/", sha256)
-	request, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return reputation{}, err
-	}
-
-	request.Header.Set("user-agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Mobile Safari/537.36")
-	request.Header.Set("referer", "https://google.com/")
-	request.Header.Set("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
-	request.Header.Set("accept-language", "en-US,en;q=0.8,he;q=0.6")
-	request.Header.Set("cache-control", "max-age=0")
-	request.Header.Set("dnt", "1")
-	request.Header.Set("upgrade-insecure-requests", "1")
-
-	response, err := http.DefaultClient.Do(request)
-	if err != nil {
-		return reputation{}, err
-	}
-
-	xmlRoot, err := xmlpath.ParseHTML(response.Body)
-	defer response.Body.Close()
-	if err != nil {
-		return reputation{}, err
-	}
-
-	scoreXPath := xmlpath.MustCompile(`//*[@id="basic-info"]/div/div[1]/table/tbody/tr[3]/td[2]`)
-	dateXPath := xmlpath.MustCompile(`//*[@id="basic-info"]/div/div[1]/table/tbody/tr[4]/td[2]`)
-	score, okScore := scoreXPath.String(xmlRoot)
-	date, okDate := dateXPath.String(xmlRoot)
-	if !okDate || !okScore {
-		return reputation{}, fmt.Errorf("Not found on VirusTotal")
-	}
-
-	date = strings.Trim(date, " \n")[:19]
-
-	score = strings.Replace(strings.Trim(score, " \n"), " ", "", -1)
-	scoreSplited := strings.Split(score, "/")
-	if len(scoreSplited) != 2 {
-		return reputation{Known: true, Confidence: 1, Date: date, Source: "VirusTotal"}, fmt.Errorf("Error parsing score")
-	}
-
-	detectedStr, totalStr := scoreSplited[0], scoreSplited[1]
-	detected, err := strconv.Atoi(detectedStr)
-	if err != nil {
-		return reputation{Known: true, Confidence: 1, Date: date, Source: "VirusTotal"}, fmt.Errorf("Error parsing score")
-	}
-
-	if detected == 0 {
-		return reputation{
-			Good:       true,
-			Confidence: 1,
-			Date:       date,
-			Source:     "VirusTotal"}, nil
-	}
-
-	total, err := strconv.Atoi(totalStr)
-	if err != nil {
-		return reputation{Known: true, Confidence: 1, Date: date, Source: "VirusTotal"}, fmt.Errorf("Error parsing score")
-	}
-
-	return reputation{
-		Bad:        true,
-		Confidence: float64(detected) / float64(total),
-		Date:       date,
-		Source:     "VirusTotal"}, nil
-
 }
---
+++ virustotal.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+
+	xmlpath "gopkg.in/xmlpath.v2"
+)
+
+type virustotal struct{}
+
+func (vt virustotal) CheckSHA256(sha256 string) (reputation, error) {
+	url := fmt.Sprintf("https://www.virustotal.com/en/file/%s/analysis/", sha256)
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return reputation{}, err
+	}
+
+	request.Header.Set("user-agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Mobile Safari/537.36")
+	request.Header.Set("referer", "https://google.com/")
+	request.Header.Set("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+	request.Header.Set("accept-language", "en-US,en;q=0.8,he;q=0.6")
+	request.Header.Set("cache-control", "max-age=0")
+	request.Header.Set("dnt", "1")
+	request.Header.Set("upgrade-insecure-requests", "1")
+
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return reputation{}, err
+	}
+
+	xmlRoot, err := xmlpath.ParseHTML(response.Body)
+	defer response.Body.Close()
+	if err != nil {
+		return reputation{}, err
+	}
+
+	scoreXPath := xmlpath.MustCompile(`//*[@id="basic-info"]/div/div[1]/table/tbody/tr[3]/td[2]`)
+	dateXPath := xmlpath.MustCompile(`//*[@id="basic-info"]/div/div[1]/table/tbody/tr[4]/td[2]`)
+	score, okScore := scoreXPath.String(xmlRoot)
+	date, okDate := dateXPath.String(xmlRoot)
+	if !okDate || !okScore {
+		return reputation{}, fmt.Errorf("Not found on VirusTotal")
+	}
+
+	date = strings.Trim(date, " \n")[:19]
+
+	score = strings.Replace(strings.Trim(score, " \n"), " ", "", -1)
+	scoreSplited := strings.Split(score, "/")
+	if len(scoreSplited) != 2 {
+		return reputation{
+			Known:  true,
+			Date:   date,
+			Source: "VirusTotal"}, fmt.Errorf("Error parsing score")
+	}
+
+	detectedStr, totalStr := scoreSplited[0], scoreSplited[1]
+	detected, err := strconv.Atoi(detectedStr)
+	if err != nil {
+		return reputation{
+			Known:  true,
+			Date:   date,
+			Source: "VirusTotal"}, fmt.Errorf("Error parsing score")
+	}
+
+	if detected == 0 {
+		return reputation{
+			Good:       true,
+			Confidence: 1,
+			Date:       date,
+			Source:     "VirusTotal"}, nil
+	}
+
+	total, err := strconv.Atoi(totalStr)
+	if err != nil {
+		return reputation{
+			Known:  true,
+			Date:   date,
+			Source: "VirusTotal"}, fmt.Errorf("Error parsing score")
+	}
+
+	return reputation{
+		Bad:        true,
+		Confidence: float64(detected) / float64(total),
+		Date:       date,
+		Source:     "VirusTotal"}, nil
+}
